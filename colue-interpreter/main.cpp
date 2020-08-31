b9f7bedbf12d5e8a59c59cfa8c3cce6d1a3b3cb0
WhiteNight <1344209351@qq.com>
2020-08-31 11:55:46 +0800

    合并了一个代码优化，添加了符号表类，标识符类，符号表操作返回值的枚举类，将语言标准设置为latest，启用了module的实验性功能。

--- colue-interpreter/main.cpp
+++ colue-interpreter/main.cpp
@@ -1,24 +1,52 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <sstream>
+#include <filesystem>
+#include <unordered_map>
+#include <utility>
 
-// vars
-std::string source_file_str;
+enum class SymbolTableResults { kUnknownError = -1, kError, kSuccess };
+
+class Identifier	// 标识符。
+{
+	std::string name_;
+	std::string value_;
+	std::string type_;
+	
+
+public:
+	Identifier(std::string key, std::string value, std::string type) :
+		name_(std::move(key)),
+		value_(std::move(value)),
+		type_(std::move(type)) {}
+
+	friend class SymbolTable;
+};
+
+class SymbolTable	// 符号表。将被实例化为 一个全局符号表 与 各调用帧的符号表 。
+{
+	std::unordered_map<std::string, Identifier> identifiers_;
+	
+public:
+	auto insert(std::string key, std::string value, std::string type)
+	{
+		return SymbolTableResults::kUnknownError;
+	}
+};
 
 // functions
-void ReadSourceFileToStr()
+std::string ReadSourceFileToStr(const std::filesystem::path& file_path)
 {
-	const auto* file_address = "F:/test.cb";
-	std::fstream source_file(file_address, std::ios::in);
+	std::fstream source_file(file_path, std::ios::in);
 	std::stringstream buf;
 	buf << source_file.rdbuf();
-	source_file_str = buf.str();
-	source_file.close();
+	return buf.str();
 }
 
 int main(int argc, char* argv[])
 {
-	ReadSourceFileToStr();
-	std::cout << source_file_str;
+	const auto* file_address = "F:/test.cb";
+	const auto src = ReadSourceFileToStr(file_address);
+	std::cout << src;
 	return 0;
 }
